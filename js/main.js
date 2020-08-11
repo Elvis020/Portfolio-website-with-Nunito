a7a417584a303c0c4b69e46f28c1ec0acc8edb0b
Elvis020 <elvis.amoako@stu.ucc.edu.gh>
2020-08-11 07:09:02 +0100

    Added AOS and animate scroll

--- js/main.js
+++ js/main.js
@@ -1,6 +1,37 @@
 $(document).ready(function () {
+
+  // 😜 Toggling the nav button
   $('.menu-toggler').on('click', function () {
     $(this).toggleClass('open');
     $('.top-nav').toggleClass('open');
   });
+
+  // 😜 Toggling-off the nav button
+  $('.top-nav').on('click', function () {
+    $('.menu-toggler').removeClass('open');
+    $('.top-nav').removeClass('open');
+  });
+
+  // Animating scrolling when you click on a nav link
+  $('nav a[href*="#"]').on('click', function () {
+    $('html, body').animate({
+      scrollTop: $($(this).attr('href')).offset().top - 100
+    }, 2000);
+  });
+
+
+  // The pulsating button with animation
+  $('#up').on('click', function () {
+    $('html, body').animate({
+      scrollTop: 0
+    }, 2000);
+  });
+
+
+  AOS.init({
+    easing: 'ease',
+    duration: 1800,
+    once: true
+  })
+
 });
